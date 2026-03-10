96c5bb73dbfdcb79837ce45dc459d4318122dbcc
fannykokopopz <fannyko.yifan@gmail.com>
2026-03-11 00:33:42 +0800

    Update social_listening.py

--- social_listening.py
+++ social_listening.py
@@ -27,7 +27,7 @@ level=logging.INFO,
 format="%(asctime)s  %(levelname)-8s  %(message)s",
 datefmt="%Y-%m-%d %H:%M:%S",
 )
-log = logging.getLogger(**name**)
+log = logging.getLogger(__name__)
 
 # ── Config ─────────────────────────────────────────────────────────────────────
 
